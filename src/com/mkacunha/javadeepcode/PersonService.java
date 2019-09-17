085ff4fc3d82589fb2d32a71fbc43660b86cf607
Maiko Cunha <mkacunha@gmail.com>
2019-09-16 22:33:46 -0300

    Added bug

--- src/com/mkacunha/javadeepcode/PersonService.java
+++ src/com/mkacunha/javadeepcode/PersonService.java
@@ -4,6 +4,8 @@ public class PersonService {
 
 
     private boolean save(Person person) {
+        String name;
+        person.name = name.toUpperCase();
         if (!new PersonRepository().exists(person.getName())) {
             new PersonRepository().save(person);
             return true;
